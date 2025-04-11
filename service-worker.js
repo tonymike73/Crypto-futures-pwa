76dfd2627a8bc45136f6040b59fc5272d19954de
tonymike73 <tonymike73@gmail.com>
2025-04-11 06:49:16 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,11 @@
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(caches.open('futures-calculator').then(function(cache) {
+    return cache.addAll(['./index.html']);
+  }));
+});
+self.addEventListener('fetch', function(e) {
+  e.respondWith(caches.match(e.request).then(function(response) {
+    return response || fetch(e.request);
+  }));
+});
